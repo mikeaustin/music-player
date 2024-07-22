695109769095ce44443d069ad63ce9a4fc53a21e
Mike Austin <mike_ekim@yahoo.com>
2024-07-22 15:42:23 -0700

    Add Dial component; Move components into their own function

--- packages/music-player/src/App.tsx
+++ packages/music-player/src/App.tsx
@@ -2,6 +2,7 @@ import { JSX, ComponentProps, createSignal, createEffect, splitProps } from 'sol
 import { parseBuffer } from 'music-metadata';
 
 import { View, Button, Text } from './core';
+import Dial from './components/Dial';
 
 import styles from './App.module.css';
 
@@ -22,7 +23,12 @@ function Component(
   );
 }
 
-function App() {
+type DATPlayerProps = {
+  audioContext: AudioContext;
+  file: File | null;
+};
+
+function DATPlayer(props: DATPlayerProps) {
   const [songTitle, setSongTitle] = createSignal('');
   const [albumTitle, setAlbumTitle] = createSignal('');
   const [artistName, setArtistName] = createSignal('');
@@ -32,30 +38,18 @@ function App() {
   const [channelsCount, setChannelsCount] = createSignal(0);
   const [currentTime, setCurrentTime] = createSignal(0);
 
-  const audioContext = new AudioContext();
-
-  const handleDragOver: JSX.EventHandler<HTMLDivElement, DragEvent> = (event) => {
-    event.preventDefault();
-  };
-
-  const handleDrop: JSX.EventHandler<HTMLDivElement, DragEvent> = async (event) => {
-    event.preventDefault();
-
-    const file = event.dataTransfer?.items[0]?.getAsFile();
-
-    if (file) {
-      const track = new AudioBufferSourceNode(audioContext, {
-        buffer: await audioContext.decodeAudioData(await file.arrayBuffer()),
+  createEffect(async () => {
+    if (props.file) {
+      const track = new AudioBufferSourceNode(props.audioContext, {
+        buffer: await props.audioContext.decodeAudioData(await props.file.arrayBuffer()),
       });
 
-      const metadata = await parseBuffer(new Uint8Array(await file.arrayBuffer()));
+      const metadata = await parseBuffer(new Uint8Array(await props.file.arrayBuffer()));
 
       console.log(metadata);
 
       setInterval(() => {
-        // console.log(audioContext.currentTime / songLength());
-
-        setCurrentTime(audioContext.currentTime);
+        setCurrentTime(props.audioContext.currentTime);
       }, 1000);
 
       metadata.common.title && setSongTitle(metadata.common.title);
@@ -67,10 +61,152 @@ function App() {
       metadata.format.numberOfChannels && setChannelsCount(metadata.format.numberOfChannels);
 
       track
-        .connect(audioContext.destination);
+        .connect(props.audioContext.destination);
 
       track.start();
     }
+  });
+
+  return (
+    <Component horizontal>
+      <View padding="large xlarge">
+        <Button align="top center" width="80px" height="50px" padding="small none" style={{ appearance: 'none', background: 'none', border: '2px solid black', "border-radius": '4px' }}>
+          <View width="20px" height="3px" style={{ background: 'hsl(200, 90%, 60%)' }} />
+        </Button>
+      </View>
+      <View padding="large large" style={{ background: 'black', width: '600px', 'border-left': '1px solid black', 'border-right': '1px solid black' }}>
+        <View style={{ position: 'absolute', inset: 0, background: 'linear-gradient(hsl(0, 0%, 0%), hsl(0, 0%, 5%) 50px, hsl(0, 0%, 0%) 150px) 0px 0px / 100% 300px no-repeat' }} />
+        <View horizontal>
+          <Text>
+            {bitsPerSample()} BIT &nbsp; {sampleRate() / 1000} KHZ
+          </Text>
+          <View flex />
+          <Text>
+            {channelsCount() === 2 ? 'STEREO' : 'MONO'}
+          </Text>
+        </View>
+        <View flex />
+        <View>
+          <Text>
+            {songTitle().toUpperCase()}
+          </Text>
+          <View height="8px" />
+          <Text>
+            {artistName() && (
+              <>
+                {artistName().toUpperCase()} — {albumTitle().toUpperCase()}
+              </>
+            )}
+          </Text>
+        </View>
+        <View flex />
+        <View>
+          <View horizontal style={{ height: '2px', background: 'hsla(200, 90%, 60%, 0.5)' }}>
+            <View width={`${currentTime() / songLength() * 100}%`} style={{ background: 'hsl(200, 90%, 60%)' }} />
+          </View>
+          <View height="8px" />
+          <View horizontal>
+            <Text>
+              {Math.floor(currentTime() / 60)}:{`${Math.floor(currentTime() % 60)}`.padStart(2, '0')}
+            </Text>
+            <View flex />
+            <Text>
+              {Math.floor(songLength() / 60)}:{`${Math.floor(songLength() % 60)}`.padStart(2, '0')}
+            </Text>
+          </View>
+        </View>
+      </View>
+    </Component>
+  );
+}
+
+type ReceiverProps = {
+  audioContext: AudioContext;
+  file: File | null;
+};
+
+function Receiver(props: ReceiverProps) {
+  const handleVolumeValueChange = (volume: number) => {
+
+  };
+
+  const gainNode = new GainNode(props.audioContext);
+
+  return (
+    <Component horizontal>
+      <View padding="large xlarge">
+        <Button align="top center" width="80px" height="50px" padding="small none" style={{ appearance: 'none', background: 'none', border: '2px solid black', "border-radius": '4px' }}>
+          <View width="20px" height="3px" style={{ background: 'hsl(200, 90%, 60%)' }} />
+        </Button>
+      </View>
+      <View padding="large large" style={{ background: 'black', width: '600px', 'border-left': '1px solid black', 'border-right': '1px solid black' }}>
+        <View style={{ position: 'absolute', inset: 0, background: 'linear-gradient(hsl(0, 0%, 0%), hsl(0, 0%, 5%) 50px, hsl(0, 0%, 0%) 150px) 0px 0px / 100% 300px no-repeat' }} />
+      </View>
+      <View flex horizontal padding="large xlarge">
+        <View flex />
+        <Dial size={150} initialValue={0.5} onValueChange={handleVolumeValueChange} />
+      </View>
+    </Component>
+  );
+}
+
+async function createDATPlayerComponent(audioContext: AudioContext, file: File | null) {
+  const track = new AudioBufferSourceNode(audioContext, {
+    buffer: await audioContext.decodeAudioData(await file.arrayBuffer()),
+  });
+
+  return {
+    node: track,
+    element: <DATPlayer audioContext={audioContext} file={file} />
+  };
+}
+
+class DATPlayerComponent {
+  static async create(audioContext: AudioContext, file: File | null) {
+    if (file) {
+      const audioNode = new AudioBufferSourceNode(audioContext, {
+        buffer: await audioContext.decodeAudioData(await file.arrayBuffer()),
+      });
+
+      return new DATPlayerComponent(audioNode, file);
+    }
+  }
+
+  // audioNode: AudioNode;
+  element: JSX.Element;
+
+  constructor(audioNode: AudioNode, public file: File | null) {
+    this.element = <Receiver audioNode={audioNode} file={file} />;
+  }
+}
+
+class ReceiverComponent {
+  audioNode: AudioNode;
+  element: JSX.Element;
+
+  constructor(public audioContext: AudioContext, public file: File | null) {
+    this.audioNode = new GainNode(audioContext);
+    this.element = <Receiver audioContext={audioContext} file={file} />;
+  }
+}
+
+function App() {
+  const [file, setFile] = createSignal<File | null>(null);
+
+  const audioContext = new AudioContext();
+
+  const datplayer = DATPlayerComponent.create(audioContext, file());
+
+  const handleDragOver: JSX.EventHandler<HTMLDivElement, DragEvent> = (event) => {
+    event.preventDefault();
+  };
+
+  const handleDrop: JSX.EventHandler<HTMLDivElement, DragEvent> = async (event) => {
+    event.preventDefault();
+
+    const file = event.dataTransfer?.items[0]?.getAsFile() ?? null;
+
+    setFile(file);
   };
 
   return (
@@ -81,59 +217,8 @@ function App() {
       onDragOver={handleDragOver}
       onDrop={handleDrop}
     >
-      <Component horizontal>
-        <View padding="large xlarge">
-          <Button align="top center" width="80px" height="50px" padding="small none" style={{ appearance: 'none', background: 'none', border: '2px solid black', "border-radius": '4px' }}>
-            <View width="20px" height="3px" style={{ background: 'hsl(200, 90%, 60%)' }} />
-          </Button>
-        </View>
-        <View padding="large large" style={{ background: 'black', width: '600px', 'border-left': '1px solid black', 'border-right': '1px solid black' }}>
-          <View style={{ position: 'absolute', inset: 0, background: 'linear-gradient(hsl(0, 0%, 0%), hsl(0, 0%, 5%) 50px, hsl(0, 0%, 0%) 150px) 0px 0px / 100% 300px no-repeat' }} />
-          <View horizontal>
-            <Text>
-              {bitsPerSample()} BIT &nbsp; {sampleRate() / 1000} KHZ
-            </Text>
-            <View flex />
-            <Text>
-              {channelsCount() === 2 ? 'STEREO' : 'MONO'}
-            </Text>
-          </View>
-          <View flex />
-          <View>
-            <Text>
-              {songTitle().toUpperCase()}
-            </Text>
-            <View height="8px" />
-            <Text>
-              {artistName() && (
-                <>
-                  {artistName().toUpperCase()} — {albumTitle().toUpperCase()}
-                </>
-              )}
-            </Text>
-          </View>
-          <View flex />
-          <View>
-            <View horizontal style={{ height: '3px', background: 'hsla(200, 90%, 60%, 0.5)' }}>
-              <View width={`${currentTime() / songLength() * 100}%`} style={{ background: 'hsl(200, 90%, 60%)' }} />
-            </View>
-            <View height="8px" />
-            <View horizontal>
-              <Text>
-                {Math.floor(currentTime() / 60)}:{`${Math.floor(currentTime() % 60)}`.padStart(2, '0')}
-              </Text>
-              <View flex />
-              <Text>
-                {Math.floor(songLength() / 60)}:{`${Math.floor(songLength() % 60)}`.padStart(2, '0')}
-              </Text>
-            </View>
-          </View>
-        </View>
-      </Component>
-      <Component horizontal>
-        <View>hello</View>
-        <View>hello</View>
-      </Component>
+      <DATPlayer audioContext={audioContext} file={file()} />
+      <Receiver audioContext={audioContext} file={file()} />
     </View>
   );
 }
---
+++ packages/music-player/src/components/Dial.tsx
@@ -0,0 +1,82 @@
+import { JSX, ComponentProps, createSignal, createEffect, splitProps } from 'solid-js';
+
+import { View, Button, Text } from '../core';
+
+function Dial(props: {
+  size: number;
+  initialValue?: number;
+  onValueChange?: (value: number) => void;
+}) {
+  const [value, setValue] = createSignal<number>(props.initialValue ?? 0.5);
+  const [firstEvent, setFirstEvent] = createSignal<PointerEvent>();
+
+  let _value: number;
+
+  const handlePointerDown: JSX.EventHandler<HTMLDivElement, PointerEvent> = (event) => {
+    event.currentTarget.setPointerCapture(event.pointerId);
+
+    _value = value();
+
+    setFirstEvent(event);
+  };
+
+  const handlePointerMove: JSX.EventHandler<HTMLDivElement, PointerEvent> = (event) => {
+    const _firstEvent = firstEvent();
+
+    if (_firstEvent) {
+      const value = Math.max(0.0, Math.min(1.0, _value + (_firstEvent.clientY - event.clientY) / 270));
+
+      setValue(value);
+
+      props.onValueChange?.(value);
+    }
+  };
+
+  const handlePointerUp: JSX.EventHandler<HTMLDivElement, PointerEvent> = () => {
+    setFirstEvent(undefined);
+  };
+
+  const handleWheel: JSX.EventHandler<HTMLDivElement, WheelEvent> = (event) => {
+    console.log('handleWheel');
+
+    _value = value();
+
+    const value2 = Math.max(0.0, Math.min(1.0, _value - Math.sign(event.deltaY) / 50));
+
+    setValue(value2);
+
+    props.onValueChange?.(value2);
+  };
+
+  return (
+    <View>
+      <View
+        style={{
+          position: 'absolute',
+          inset: 0,
+          "border-radius": '9999px',
+          'box-shadow': '0 5px 10px hsla(0, 0%, 0%, 0.5), 0 0 0 2px hsl(0, 0%, 0%)',
+        }}
+      />
+      <View
+        align="top center"
+        style={{
+          width: `${props.size}px`,
+          height: `${props.size}px`,
+          border: '1px solid hsl(0, 0%, 10%)',
+          "border-radius": '9999px',
+          background: 'radial-gradient(hsl(0, 0%, 2%), hsl(0, 0%, 5%))',
+          transform: `rotate(${value() * 270 - 135}deg)`
+        }}
+        onPointerDown={handlePointerDown}
+        onPointerMove={handlePointerMove}
+        onPointerUp={handlePointerUp}
+        onWheel={handleWheel}
+      >
+        <View style={{ background: 'hsl(200, 90%, 60%)', width: '3px', height: '20px' }} />
+      </View>
+    </View>
+  );
+}
+
+export default Dial;
