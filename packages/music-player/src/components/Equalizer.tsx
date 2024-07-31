17abc557fa86532c4dd59fc0e5f8d57872b4dfa1
Mike Austin <mike_ekim@yahoo.com>
2024-07-30 17:03:51 -0700

    Work more on Equalizer component

--- packages/music-player/src/components/Equalizer.tsx
+++ packages/music-player/src/components/Equalizer.tsx
@@ -6,6 +6,55 @@ import Dial from '../components/Dial';
 import Button from '../components/Button';
 import Component from '../components/Component';
 
+function Slider() {
+  const [value, setValue] = createSignal<number>(0.5);
+  const [firstEvent, setFirstEvent] = createSignal<PointerEvent>();
+
+  let _value: number;
+
+  const height = 150 - 56;
+
+  const handlePointerDown: JSX.EventHandler<HTMLDivElement, PointerEvent> = (event) => {
+    event.currentTarget.setPointerCapture(event.pointerId);
+
+    _value = value();
+
+    setFirstEvent(event);
+  };
+
+  const handlePointerMove: JSX.EventHandler<HTMLDivElement, PointerEvent> = (event) => {
+    const _firstEvent = firstEvent();
+
+    if (_firstEvent) {
+      const value = Math.max(0.0, Math.min(1.0, _value + (_firstEvent.clientY - event.clientY) / height));
+
+      setValue(value);
+
+      // props.onValueChange?.(value);
+    }
+  };
+
+  const handlePointerUp: JSX.EventHandler<HTMLDivElement, PointerEvent> = () => {
+    setFirstEvent(undefined);
+  };
+
+  return (
+    <View height="150px" style={{ xbackground: 'black', xborder: '2px solid black', "border-radius": '4px' }}>
+      <View style={{ position: 'absolute', inset: '0 10px 0 9px', background: 'black', "border-radius": '4px' }} />
+      <View
+        padding="large none"
+        style={{ background: 'hsl(0, 0%, 5%)', border: '1px solid hsl(0, 0%, 10%', "border-radius": '4px', top: `${height - value() * height}px`, "box-shadow": '0 0 0 2px black' }}
+        // title={`${value()}`}
+        onPointerDown={handlePointerDown}
+        onPointerMove={handlePointerMove}
+        onPointerUp={handlePointerUp}
+      >
+        <View style={{ background: 'hsl(200, 90%, 60%)', width: '20px', height: '3px' }} />
+      </View>
+    </View>
+  );
+}
+
 type EqualizerProps = {
   audioNode: BiquadFilterNode;
   file: File | null;
@@ -56,10 +105,21 @@ function Equalizer(props: EqualizerProps) {
           <View width="20px" height="3px" style={{ background: 'hsl(200, 90%, 60%)' }} />
         </Button>
       </View>
-      <View padding="large large" style={{ background: 'black', width: '600px', 'border-left': '2px solid black', 'border-right': '2px solid black' }}>
+      <View padding="large large" style={{ background: 'black', width: '600px', "flex-shrink": 0, 'border-left': '2px solid black', 'border-right': '2px solid black' }}>
         <View absolute style={{ inset: 0, background: 'linear-gradient(hsl(0, 0%, 0%), hsl(0, 0%, 5%) 50px, hsl(0, 0%, 0%) 150px) 0px 0px / 100% 300px no-repeat' }} />
       </View>
-      <View padding="large xlarge">
+      <View horizontal padding="large xlarge" style={{ gap: '24px' }}>
+        <Slider />
+        <Slider />
+        <Slider />
+        <Slider />
+        <Slider />
+        <Slider />
+        <Slider />
+        <Slider />
+        <Slider />
+        <Slider />
+        <Slider />
       </View>
       <View flex />
       <View horizontal padding="large xlarge" align="middle left">
